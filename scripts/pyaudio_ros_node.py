9ee342a03c2a5fed849fc5d97e8406534fed209e
Ubuntu <you@example.com>
2021-06-22 23:09:00 +0000

    pyaudio play audio node developed and tested with tts ros and is working, partial sound is playing

--- scripts/pyaudio_ros_node.py
+++ scripts/pyaudio_ros_node.py
@@ -1,49 +1,29 @@
 #!/usr/bin/env python3
 import rospy
-from pyaudio_ros.msg import Audio
+from tts_ros.msg import Audio
+
 import pyaudio
-import struct
+import numpy as np
 
-def talker():
-    pub = rospy.Publisher('audio', Audio, queue_size=10)
-    rospy.init_node('pyaudio_ros_node', anonymous=True)
-    rate = rospy.Rate(10) # 10hz
+class PyAudioRos:
+    def __init__(self):
+        self.p = pyaudio.PyAudio()
+        self.stream = self.p.open(format=pyaudio.paFloat32,channels=1,rate=22050,output=True)
+        
+        rospy.Subscriber("audio", Audio, self.callback)
    
-    sample_rate = 16000
-    frame_length = 512
-    input_device_index = None
+    def __del__(self):
+        self.stream.stop_stream()
+        self.stream.close()
 
-    if rospy.has_param('pyaudio_ros/sample_rate'):
-        sample_rate = rospy.get_param('/pyaudio_ros/sample_rate')
-    if rospy.has_param('pyaudio_ros/frame_length'):
-        frame_length = rospy.get_param('/pyaudio_ros/frame_length')
-    if rospy.has_param('pyaudio_ros/input_device_index'):
-        input_device_index = rospy.get_param('/pyaudio_ros/input_device_index')
-    
-    rospy.loginfo('sample_rate: %s', sample_rate)
-    rospy.loginfo('frame_length: %s', frame_length)
-    rospy.loginfo('input_device_index: %s', input_device_index)
+    def callback(self, audio_input):
+        audio = np.array(audio_input.data, dtype=np.float32)
+        self.stream.write(audio)
 
-    pa = pyaudio.PyAudio()
-    audio_stream = pa.open(
-                rate=sample_rate,
-                channels=1,
-                format=pyaudio.paInt16,
-                input=True,
-                frames_per_buffer=frame_length,
-                input_device_index=input_device_index)
-    audio = Audio()
-    while not rospy.is_shutdown():
-        pcm = audio_stream.read(frame_length)
-        pcm = struct.unpack_from("h" * frame_length, pcm)
-        print(len(pcm))
-        audio.AudioData.data = pcm
-        pub.publish(audio)
-        rate.sleep()
+def listener():
+    rospy.init_node('pyaudio_play')
+    pyaudio_ros = PyAudioRos()
+    rospy.spin()
 
 if __name__ == '__main__':
-    try:
-        talker()
-    except rospy.ROSInterruptException:
-        pass
-
+    listener()
