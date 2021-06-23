7e1f8aca4721926641c1ebc24108bf8d561ee9e5
Ubuntu <you@example.com>
2021-06-23 06:39:42 +0000

    paudio is now tested

--- scripts/pyaudio_ros_node.py
+++ scripts/pyaudio_ros_node.py
@@ -9,16 +9,18 @@ class PyAudioRos:
     def __init__(self):
         self.p = pyaudio.PyAudio()
         self.stream = self.p.open(format=pyaudio.paFloat32,channels=1,rate=22050,output=True)
-        
+        self.stream.start_stream()
+
         rospy.Subscriber("audio", Audio, self.callback)
    
     def __del__(self):
         self.stream.stop_stream()
         self.stream.close()
 
-    def callback(self, audio_input):
-        audio = np.array(audio_input.data, dtype=np.float32)
-        self.stream.write(audio)
+    def callback(self, audio):
+        chunk = np.array(audio.data, np.float32)
+        chunk = chunk.tobytes()
+        self.stream.write(chunk)
 
 def listener():
     rospy.init_node('pyaudio_play')
